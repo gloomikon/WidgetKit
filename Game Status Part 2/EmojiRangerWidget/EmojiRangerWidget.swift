84186e35f557bf6adbe5100c8373f0d73a2d22a6
gloomikon <gloomikon@gmail.com>
2023-10-08 17:16:19 +0200

    Part 2 - Families, Timelines, Configuration, Deep Linking

--- Game Status Part 2/EmojiRangerWidget/EmojiRangerWidget.swift
+++ Game Status Part 2/EmojiRangerWidget/EmojiRangerWidget.swift
@@ -9,20 +9,52 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), character: .panda)
+struct Provider: IntentTimelineProvider {
+
+    private func character(for configuration: CharacterSelectionIntent) -> CharacterDetail {
+        switch configuration.hero {
+        case .panda:
+                .panda
+        case .egghead:
+                .egghead
+        case .spouty:
+                .spouty
+        case .unknown:
+                .panda
+        }
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), character: .panda)
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), character: .panda, relevance: nil)
+    }
+
+    func getSnapshot(
+        for configuration: CharacterSelectionIntent,
+        in context: Context,
+        completion: @escaping (SimpleEntry) -> ()
+    ) {
+        let entry = SimpleEntry(date: Date(), character: .panda, relevance: nil)
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        let entries: [SimpleEntry] = [
-            .init(date: Date(), character: .panda)
-        ]
+    func getTimeline(
+        for configuration: CharacterSelectionIntent,
+        in context: Context,
+        completion: @escaping (Timeline<Entry>) -> ()
+    ) {
+        let selectedCharacter = character(for: configuration)
+        let endDate = selectedCharacter.fullHealthDate
+        let oneMinute: TimeInterval = 60
+        var currentDate = Date()
+
+        var entries: [SimpleEntry] = []
+
+        while currentDate < endDate {
+            let relevance = TimelineEntryRelevance(score: Float(selectedCharacter.healthLevel))
+            let entry = SimpleEntry(date: currentDate, character: selectedCharacter, relevance: relevance)
+            currentDate += oneMinute
+            entries.append(entry)
+        }
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
@@ -32,13 +64,31 @@ struct Provider: TimelineProvider {
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let character: CharacterDetail
+    let relevance: TimelineEntryRelevance?
 }
 
 struct EmojiRangerWidgetEntryView : View {
     var entry: Provider.Entry
 
+    @Environment(\.widgetFamily) var family
+
+    @ViewBuilder
     var body: some View {
-        AvatarView(entry.character)
+        switch family {
+        case .systemSmall:
+            AvatarView(entry.character)
+                .widgetURL(entry.character.url)
+        default:
+                HStack(alignment: .top) {
+                    AvatarView(entry.character)
+                    Text(entry.character.bio)
+                        .padding()
+
+                }
+                .foregroundColor(.white)
+                .widgetURL(entry.character.url)
+                .background(Color.gameBackground)
+        }
     }
 }
 
@@ -46,7 +96,11 @@ struct EmojiRangerWidget: Widget {
     let kind: String = "EmojiRangerWidget"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        IntentConfiguration(
+            kind: kind,
+            intent: CharacterSelectionIntent.self,
+            provider: Provider()
+        ) { entry in
             if #available(iOS 17.0, *) {
                 EmojiRangerWidgetEntryView(entry: entry)
                     .containerBackground(.fill.tertiary, for: .widget)
@@ -58,12 +112,18 @@ struct EmojiRangerWidget: Widget {
         }
         .configurationDisplayName("Emoji Ranger Detail")
         .description("Keep track of your favorite emoji ranger")
-        .supportedFamilies([.systemSmall])
+        .supportedFamilies([.systemSmall, .systemMedium])
     }
+}
+
+#Preview(as: .systemMedium) {
+    EmojiRangerWidget()
+} timeline: {
+    SimpleEntry(date: .now, character: .panda, relevance: nil)
 }
 
 #Preview(as: .systemSmall) {
     EmojiRangerWidget()
 } timeline: {
-    SimpleEntry(date: .now, character: .panda)
+    SimpleEntry(date: .now, character: .panda, relevance: nil)
 }
