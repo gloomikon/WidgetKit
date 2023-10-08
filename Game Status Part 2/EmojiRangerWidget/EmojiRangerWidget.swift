0e730c6a8158dd1db4ccdd9b8070e65ef5b594bf
gloomikon <gloomikon@gmail.com>
2023-10-08 16:21:17 +0200

    Part 2 - Initial commit

--- Game Status Part 2/EmojiRangerWidget/EmojiRangerWidget.swift
+++ Game Status Part 2/EmojiRangerWidget/EmojiRangerWidget.swift
@@ -1,46 +1,40 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A widget that shows the avatar for a single character.
-*/
+//
+//  EmojiRangerWidget.swift
+//  EmojiRangerWidget
+//
+//  Created by Admin on 08.10.2023.
+//  Copyright © 2023 Apple. All rights reserved.
+//
 
 import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    
-    public typealias Entry = SimpleEntry
-    
     func placeholder(in context: Context) -> SimpleEntry {
-        return SimpleEntry(date: Date(), character: .panda)
+        SimpleEntry(date: Date(), character: .panda)
     }
-    
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), character: .panda)
         completion(entry)
     }
-    
-    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> Void) {
-        let entries = [SimpleEntry(date: Date(), character: .panda)]
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        let entries: [SimpleEntry] = [
+            .init(date: Date(), character: .panda)
+        ]
+
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
-    
 }
 
 struct SimpleEntry: TimelineEntry {
-    public let date: Date
+    let date: Date
     let character: CharacterDetail
 }
 
-struct PlaceholderView: View {
-    var body: some View {
-        AvatarView(.panda)
-    }
-}
-
-struct EmojiRangerWidgetEntryView: View {
+struct EmojiRangerWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
@@ -48,28 +42,28 @@ struct EmojiRangerWidgetEntryView: View {
     }
 }
 
-@main
 struct EmojiRangerWidget: Widget {
-    private let kind: String = "EmojiRangerWidget"
+    let kind: String = "EmojiRangerWidget"
 
-    public var body: some WidgetConfiguration {
+    var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            EmojiRangerWidgetEntryView(entry: entry)
+            if #available(iOS 17.0, *) {
+                EmojiRangerWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                EmojiRangerWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
         }
-        .configurationDisplayName("Ranger Detail")
-        .description("See your favorite ranger.")
+        .configurationDisplayName("Emoji Ranger Detail")
+        .description("Keep track of your favorite emoji ranger")
         .supportedFamilies([.systemSmall])
     }
 }
 
-struct Widget_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            AvatarView(CharacterDetail.panda)
-                .previewContext(WidgetPreviewContext(family: .systemSmall))
-            
-            PlaceholderView()
-                .previewContext(WidgetPreviewContext(family: .systemSmall))
-        }
-    }
+#Preview(as: .systemSmall) {
+    EmojiRangerWidget()
+} timeline: {
+    SimpleEntry(date: .now, character: .panda)
 }
--- Game Status Part 2/README.md
+++ Game Status Part 2/README.md
@@ -1,7 +1,7 @@
-# Emoji Rangers: Part 2
+# Emoji Rangers: Part 1
 
-Enhance a widget by supporting multiple sizes, using timelines to stay up to date, and adding user-configurable properties.  
+Create a widget using WidgetKit and SwiftUI to show details of a character in a game.
 
 ## Overview
 
-- Note: This sample code project is associated with WWDC20 session [10035: Widgets Code-along, part 2: Alternate timelines](https://developer.apple.com/wwdc20/10035/).
+- Note: This sample code project is associated with WWDC20 session [10034: Widgets Code-along, part 1: The adventure begins](https://developer.apple.com/wwdc20/10034/).
